563db6a799a017da2aeb1a432441c8a3f0be392e
Srinivasan <srinivasan.raguraman@gmail.com>
2010-12-22 22:02:56 -0600

    Added support to record a step using the timer

--- src/main/java/org/notechnique/profile/StopWatch.java
+++ src/main/java/org/notechnique/profile/StopWatch.java
@@ -3,6 +3,10 @@ package org.notechnique.profile;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ListIterator;
 import java.util.concurrent.TimeUnit;
 
 import static java.math.BigDecimal.valueOf;
@@ -12,20 +16,26 @@ public class StopWatch {
     private long startTime;
     private long endTime;
     private String taskName;
+    private List<Long> timestamps = new ArrayList<Long>();
 
     public StopWatch(String taskName) {
         this.taskName = taskName;
     }
 
-    StopWatch(String taskName, long startTime, long endTime) {
+    StopWatch(String taskName, long startTime, long endTime, List<Long> timestamps) {
         this.taskName = taskName;
         this.startTime = startTime;
         this.endTime = endTime;
+        this.timestamps = timestamps;
     }
 
     public StopWatch start() {
         this.startTime = System.nanoTime();
         return this;
+    }
+
+    public void step() {
+        this.timestamps.add(System.nanoTime());
     }
 
     public StopWatch stop() {
@@ -38,14 +48,39 @@ public class StopWatch {
     }
 
     public BigDecimal getElapsedPrecision(TimeUnit timeUnit) {
+        return getPrecisionDifference(this.startTime, this.endTime, timeUnit);
+    }
+
+    private BigDecimal getPrecisionDifference(long startTime, long endTime, TimeUnit timeUnit) {
         return valueOf(endTime - startTime).divide(valueOf(NANOSECONDS.convert(1, timeUnit)));
     }
 
-    public void prettyPrint(PrintStream writeTo, TimeUnit tu) {
-        writeTo.println(String.format("Task: %s took %s %s", taskName, getElapsedPrecision(tu).toPlainString(), tu));
+    public String prettyPrintString(TimeUnit timeUnit) {
+        return new StringBuffer().append(String.format("Task: %s took ", taskName))
+                .append(getElapsedPrecision(timeUnit).toPlainString())
+                .append(timestamps.isEmpty() ? " " : " " + getElapsedIntervalString(timeUnit) + " ")
+                .append(timeUnit)
+                .toString();
     }
 
-    public Appendable prettyPrint(Appendable writeTo, TimeUnit tu) throws IOException {
-        return writeTo.append(String.format("Task: %s took %s %s", taskName, getElapsedPrecision(tu).toPlainString(), tu));
+    private String getElapsedIntervalString(TimeUnit timeUnit) {
+        List<String> intervals = new ArrayList<String>();
+        ListIterator<Long> iterator = timestamps.listIterator();
+        long lastInterval = startTime;
+        while (iterator.hasNext()) {
+            Long currentInterval = iterator.next();
+            intervals.add(getPrecisionDifference(lastInterval, currentInterval, timeUnit).toPlainString());
+            lastInterval = currentInterval;
+        }
+        intervals.add(getPrecisionDifference(lastInterval, this.endTime, timeUnit).toPlainString());
+        return Arrays.toString(intervals.toArray());
+    }
+
+    public Appendable prettyPrint(Appendable writeTo, TimeUnit timeUnit) throws IOException {
+        return writeTo.append(prettyPrintString(timeUnit));
+    }
+
+    public void prettyPrint(PrintStream ps, TimeUnit timeUnit) {
+        ps.println(prettyPrintString(timeUnit));
     }
 }
--- src/test/java/org/notechnique/profile/StopWatchTest.java
+++ src/test/java/org/notechnique/profile/StopWatchTest.java
@@ -4,8 +4,10 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.io.StringWriter;
+import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
+import static java.util.Arrays.asList;
 import static java.util.concurrent.TimeUnit.*;
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertSame;
@@ -14,31 +16,49 @@ import static junit.framework.Assert.assertTrue;
 public class StopWatchTest {
 
     @Test
-    public void testStopWatch_getElapsed() throws InterruptedException {
-        StopWatch watch =  new StopWatch("halo");
+    public void testStopWatch_getElapsed() throws InterruptedException, IOException {
+        StopWatch watch = new StopWatch("task1");
         watch.start();
         Thread.sleep(10);
         watch.stop();
         assertTrue("Elapsed time profiled should be greater than actual Thread.sleep", watch.getElapsed(MILLISECONDS) >= 10L);
+        watch.prettyPrint(System.out, TimeUnit.MILLISECONDS);
+    }
+
+    @Test
+    public void testStopWatch_getElapsed_withStep() throws InterruptedException, IOException {
+        StopWatch watch = new StopWatch("task2");
+        watch.start();
+        Thread.sleep(10);
+        watch.step();
+        Thread.sleep(10);
+        watch.stop();
+        assertTrue("Elapsed time profiled should be greater than actual Thread.sleep", watch.getElapsed(MILLISECONDS) >= 20);
+        watch.prettyPrint(System.out, TimeUnit.MILLISECONDS);
     }
 
     @Test
     public void testStopWatch_getPrecision() throws InterruptedException {
-        StopWatch watch =  new StopWatch("", 100, 200);
+        StopWatch watch = new StopWatch("", 100, 200, new ArrayList());
         assertEquals("100", watch.getElapsedPrecision(NANOSECONDS).toPlainString());
     }
 
     @Test
     public void testStopWatch_getPrecision_toMilliSeconds() throws InterruptedException {
-        StopWatch watch =  new StopWatch("", 1000, 2000);
+        StopWatch watch = new StopWatch("", 1000, 2000, new ArrayList());
         assertEquals("0.001", watch.getElapsedPrecision(MILLISECONDS).toPlainString());
     }
 
     @Test
     public void testStopWatch_prettyPrint() throws InterruptedException, IOException {
-        StopWatch watch =  new StopWatch("profiling", 100, 200);
+        StopWatch watch = new StopWatch("profiling", 100, 200, new ArrayList());
         assertEquals("Task: profiling took 0.0001 MILLISECONDS", watch.prettyPrint(new StringWriter(), MILLISECONDS).toString());
-        watch.prettyPrint(System.out, MILLISECONDS);
+    }
+
+    @Test
+    public void testStopWatch_prettyPrint_WithIntervals() throws InterruptedException, IOException {
+        StopWatch watch = new StopWatch("profiling", 100, 200, asList(140L, 180L));
+        assertEquals("Task: profiling took 100 [40, 40, 20] NANOSECONDS", watch.prettyPrint(new StringWriter(), NANOSECONDS).toString());
     }
 
 }
